10bd945b1bd4b40f4a76abc4775bc5d879ed81d0
parbhumahat <140318413+parbhumahat@users.noreply.github.com>
2023-12-27 23:53:22 +0545

    Add files via upload

---
+++ login/admin/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Sandhya Bhandari',
+        productNumber: '9804430709',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'Karan Sharma',
+        productNumber: '9748704230',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Anshu Gyawali',
+        productNumber: '9766303031',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
